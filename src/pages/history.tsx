adb566c301cf338f8185f2319fc7fafaa6de49ef
Getaji <t.tanaka.onion@gmail.com>
2022-11-29 15:21:43 +0900

    table: 譜面追加・更新 - various: Milk   - add: !? [for patient player] [for patient player]   - add: ≡ [White]   - change: ☆?→★20 [Dream]   - change: ★13→★14 [KAHLUA]

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -16,17 +16,66 @@ const Page = () => {
         <article>
           <section>
             <h1>2022/11/29</h1>
+            <h2>楽曲追加</h2>
             <ul>
-              <li>楽曲追加: Merry Christmas Mr.Lawrence</li>
-              <li>
-                難易度変更:
-                <ul>
-                  <li>☆4→!? Aleph-0 [BEGINNER]</li>
-                  <li>★★2→!? Anger Control [Angry]</li>
-                  <li>☆?→!? Door Nock[enough]</li>
-                </ul>
-              </li>
+              <li>Merry Christmas Mr.Lawrence</li>
             </ul>
+            <h2>譜面追加</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th>難易度</th>
+                  <th>譜面</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>!?</td>
+                  <td>Milk [for patient player]</td>
+                </tr>
+                <tr>
+                  <td>≡</td>
+                  <td>Milk [White]</td>
+                </tr>
+              </tbody>
+            </table>
+            <h2>難易度変更</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th>旧</th>
+                  <th>新</th>
+                  <th>譜面</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>☆4</td>
+                  <td>!?</td>
+                  <td>Aleph-0 [BEGINNER]</td>
+                </tr>
+                <tr>
+                  <td>★★2</td>
+                  <td>!?</td>
+                  <td>Anger Control [Angry]</td>
+                </tr>
+                <tr>
+                  <td>☆?</td>
+                  <td>!?</td>
+                  <td>Door Nock[enough]</td>
+                </tr>
+                <tr>
+                  <td>☆?</td>
+                  <td>★20</td>
+                  <td>Milk [Dream]</td>
+                </tr>
+                <tr>
+                  <td>★13</td>
+                  <td>★14</td>
+                  <td>Milk [KAHLUA]</td>
+                </tr>
+              </tbody>
+            </table>
           </section>
         </article>
       </main>
