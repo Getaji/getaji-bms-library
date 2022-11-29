e9abcef6c4a973c22f4d176d2c74081c65c9cd38
Getaji <t.tanaka.onion@gmail.com>
2022-11-29 21:55:09 +0900

    table: 譜面追加・更新 - add: Minimalized Material - add: Musica de Aves - add: My Canvas - various: Nhelv   - add: ★★6 [SF HELL]   - change: ★6→★5 [ANOTHER]   - change: ☆?→★12 [Mind]   - change: ★11→★12 [pursuit] - add: Noblesse Gloria - add: Nuts to You! - various: One Way Street   - add: ◎ [皿街道]   - change: ☆?→!? [Emotional]   - change: ☆10→☆11 [Season] - add: Ordeal - add: Outernoid - various: ozma   - add: ★★5 -RAINBOW ANOTHER-   - add: !? (多様性)   - add: ★17 [Access]   - add: ★★5 [Administrator]   - add: !? [gazma]   - add: ★★4 [GOD]   - add: ★★4 [INVADER]   - change: ★6→★12 *ANOTHER 12*   - change: ★1→★★3 *INSANE 13*   - change: ★19→★18 -麒麟-   - change: ★★3→★★4 ( Θ )   - change: ★★2→★★3 [Nervous Breakdown]   - change: ★9→★8 [Seeds of Rebellion] - various: Pangaea   - add: ★★3 (おに)   - add: ★6 [7World]   - add: ★15 [7World EX]   - add: ★4 [ANOTHER]   - add: ★2 [Another+]   - add: ◆ [B-A-D LN]   - add: ◆ [HYPER LN7]   - add: ★1 [Nether]   - add: ★15 [Proxima]   - add: ★14 [tintin--]   - add: ★★2 [Ultima]   - add: ◆ [楽園]   - change: ☆3→☆2 [7keys Easy]   - change: ★10→★9 [SABOTHER]

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -19,13 +19,20 @@ const Page = () => {
             <h2>楽曲追加</h2>
             <ul>
               <li>Merry Christmas Mr.Lawrence</li>
+              <li>Minimalized Material</li>
+              <li>Musica de Aves</li>
+              <li>My Canvas</li>
+              <li>Noblesse Gloria</li>
+              <li>Nuts to You!</li>
+              <li>Ordeal</li>
+              <li>Outernoid</li>
             </ul>
             <h2>譜面追加</h2>
             <table>
               <thead>
                 <tr>
-                  <th>難易度</th>
-                  <th>譜面</th>
+                  <th style={{wordBreak: "keep-all"}}>難易度</th>
+                  <th style={{width: "100%"}}>譜面</th>
                 </tr>
               </thead>
               <tbody>
@@ -37,15 +44,99 @@ const Page = () => {
                   <td>≡</td>
                   <td>Milk [White]</td>
                 </tr>
+                <tr>
+                  <td>★★6</td>
+                  <td>Nhelv [SF HELL]</td>
+                </tr>
+                <tr>
+                  <td>◎</td>
+                  <td>One Way Street [皿街道]</td>
+                </tr>
+                <tr>
+                  <td>★★5</td>
+                  <td>ozma -RAINBOW ANOTHER-</td>
+                </tr>
+                <tr>
+                  <td>!?</td>
+                  <td>ozma (多様性)</td>
+                </tr>
+                <tr>
+                  <td>★17</td>
+                  <td>ozma [Access]</td>
+                </tr>
+                <tr>
+                  <td>★★5</td>
+                  <td>ozma [Administrator]</td>
+                </tr>
+                <tr>
+                  <td>!?</td>
+                  <td>ozma [gazma]</td>
+                </tr>
+                <tr>
+                  <td>★★4</td>
+                  <td>ozma [GOD]</td>
+                </tr>
+                <tr>
+                  <td>★★4</td>
+                  <td>ozma [INVADER]</td>
+                </tr>
+                <tr>
+                  <td>★★3</td>
+                  <td>Pangaea (おに)</td>
+                </tr>
+                <tr>
+                  <td>★6</td>
+                  <td>Pangaea [7World]</td>
+                </tr>
+                <tr>
+                  <td>★15</td>
+                  <td>Pangaea [7World EX]</td>
+                </tr>
+                <tr>
+                  <td>★4</td>
+                  <td>Pangaea [ANOTHER]</td>
+                </tr>
+                <tr>
+                  <td>★2</td>
+                  <td>Pangaea [Another+]</td>
+                </tr>
+                <tr>
+                  <td>◆</td>
+                  <td>Pangaea [B-A-D LN]</td>
+                </tr>
+                <tr>
+                  <td>◆</td>
+                  <td>Pangaea [HYPER LN7]</td>
+                </tr>
+                <tr>
+                  <td>★1</td>
+                  <td>Pangaea [Nether]</td>
+                </tr>
+                <tr>
+                  <td>★15</td>
+                  <td>Pangaea [Proxima]</td>
+                </tr>
+                <tr>
+                  <td>★14</td>
+                  <td>Pangaea [tintin--]</td>
+                </tr>
+                <tr>
+                  <td>★★2</td>
+                  <td>Pangaea [Ultima]</td>
+                </tr>
+                <tr>
+                  <td>◆</td>
+                  <td>Pangaea [楽園]</td>
+                </tr>
               </tbody>
             </table>
             <h2>難易度変更</h2>
             <table>
               <thead>
                 <tr>
-                  <th>旧</th>
-                  <th>新</th>
-                  <th>譜面</th>
+                  <th style={{wordBreak: "keep-all"}}>旧</th>
+                  <th style={{wordBreak: "keep-all"}}>新</th>
+                  <th style={{width: "100%"}}>譜面</th>
                 </tr>
               </thead>
               <tbody>
@@ -73,6 +164,71 @@ const Page = () => {
                   <td>★13</td>
                   <td>★14</td>
                   <td>Milk [KAHLUA]</td>
+                </tr>
+                <tr>
+                  <td>★6</td>
+                  <td>★5</td>
+                  <td>Nhelv [ANOTHER]</td>
+                </tr>
+                <tr>
+                  <td>☆?</td>
+                  <td>★12</td>
+                  <td>Nhelv [Mind]</td>
+                </tr>
+                <tr>
+                  <td>★11</td>
+                  <td>★12</td>
+                  <td>Nhelv [pursuit]</td>
+                </tr>
+                <tr>
+                  <td>☆?</td>
+                  <td>!?</td>
+                  <td>One Way Street [Emotional]</td>
+                </tr>
+                <tr>
+                  <td>☆10</td>
+                  <td>☆11</td>
+                  <td>One Way Street [Season]</td>
+                </tr>
+                <tr>
+                  <td>★6</td>
+                  <td>★12</td>
+                  <td>Nhelv <u>(ANOTHER 12)</u></td>
+                </tr>
+                <tr>
+                  <td>★1</td>
+                  <td>★★3</td>
+                  <td>Nhelv <u>(INSANE 13)</u></td>
+                </tr>
+                <tr>
+                  <td>★19</td>
+                  <td>★18</td>
+                  <td>Nhelv -麒麟-</td>
+                </tr>
+                <tr>
+                  <td>★★3</td>
+                  <td>★★4</td>
+                  <td>Nhelv ( Θ )</td>
+                </tr>
+                <tr>
+                  <td>★★2</td>
+                  <td>★★3</td>
+                  <td>Nhelv [Nervous Breakdown]</td>
+                </tr>
+                <tr>
+                  <td>★9</td>
+                  <td>★8</td>
+                  <td>Nhelv [Seeds of Rebellion]</td>
+                </tr>
+                <tr>
+                  <td>☆3</td>
+                  <td>☆2</td>
+                  <td>Pangaea [7keys Easy]</td>
+                </tr>
+                <tr>
+                  <td>★10</td>
+                  <td>★9</td>
+                  <td>Pangaea [SABOTHER]</td>
                 </tr>
               </tbody>
             </table>
