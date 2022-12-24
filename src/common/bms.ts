e01ad972db81f64e1c732cdafad04afef6cd15c1
Getaji <t.tanaka.onion@gmail.com>
2022-12-24 19:25:35 +0900

    change: 情報の追加とスタイル調整 - add: BPM情報 - add: TOTAL値の高低 - add: 譜面要素情報(LNや地雷など) - add: 判定の色 - change: 列の幅の調整

--- src/common/bms.ts
+++ src/common/bms.ts
@@ -1,3 +1,5 @@
+import { Song } from "./types";
+
 export const makeLR2IRUrl = (md5: string) => "http://www.dream-pro.info/~lavalse/LR2IR/search.cgi?mode=ranking&bmsmd5=" + md5;
 export const makeMochaUrl = (sha256: string) => "https://mocha-repository.info/song.php?sha256=" + sha256;
 
@@ -5,13 +7,62 @@ export const JUDGE_RANK_MAP: Record<string, string> = {
   "100": "EASY",
   "75": "NORMAL",
   "50": "HARD",
-  "25": "VERY HARD",
+  "25": "VERYHARD",
 };
 
 export const getJudge = (judge: number) => {
-  if (judge <= 25) return "VERY HARD";
+  if (judge <= 25) return "VERYHARD";
   if (judge <= 50) return "HARD";
   if (judge <= 75) return "NORMAL";
   if (judge <= 100) return "EASY";
-  return "VERY EASY";
+  return "VERYEASY";
+}
+
+export function getBpmText(song: Song) {
+  if (song.minbpm === song.maxbpm) {
+    return String(song.minbpm);
+  }
+
+  return `${song.minbpm}-(${song.mainbpm})-${song.maxbpm}`;
+}
+
+export function calcTotal(notes: number) {
+  return Math.max(260.0, 7.605 * notes / (0.01 * notes + 6.5));
+}
+
+type Feature = {
+  num: number;
+  id: string;
+  jp: string;
+  en: string;
+};
+
+function feature(
+  num: number,
+  id: string,
+  jp: string,
+  en: string
+) {
+  return { num, id, jp, en };
+}
+
+const FEATURES: Feature[] = [
+  feature(1, "undefined-ln", "LN", "LN"),
+  feature(2, "minenote", "地雷", "Mine"),
+  feature(4, "random", "ランダム", "Random"),
+  feature(8, "longnote", "強制LN", "Force LN"),
+  feature(16, "chargenote", "強制CN", "Force CN"),
+  feature(32, "hellchargenote", "強制HCN", "Force HCN"),
+  feature(64, "stop-sequence", "ストップシーケンス", "Stop Sequence"),
+  feature(128, "scroll", "スクロール", "scroll"),
+];
+
+export function getFeatures(song: Song) {
+  return FEATURES.filter((feature) => (song.feature & feature.num) !== 0);
+}
+
+export function getTimeString(song: Song) {
+  const minutes = Math.floor(song.length / (60 * 1000));
+  const seconds = Math.floor(song.length % (60 * 1000) / 1000);
+  return minutes + ":" + String(seconds).padStart(2, "0");
 }
--- src/components/BMSTable.tsx
+++ src/components/BMSTable.tsx
@@ -1,6 +1,14 @@
 import React, { Fragment } from "react";
 import { Song } from "../common/types";
-import { getJudge, makeLR2IRUrl, makeMochaUrl } from "../common/bms";
+import {
+  calcTotal,
+  getBpmText,
+  getJudge,
+  getFeatures,
+  makeLR2IRUrl,
+  makeMochaUrl,
+  getTimeString,
+} from "../common/bms";
 import "./BMSTable.css";
 
 export type Props = {
@@ -53,7 +61,9 @@ export function BMSTable({ table }: Props) {
                       target="_blank"
                       rel="noopener noreferrer"
                     >
-                      LR2<br />IR
+                      LR2
+                      <br />
+                      IR
                     </a>
                     <a
                       href={makeMochaUrl(song.sha256)}
@@ -65,11 +75,34 @@ export function BMSTable({ table }: Props) {
                   </div>
                 </td>
                 <td className="info">
+                  <div className="info-bpm_notes_time">
+                    <div>BPM{getBpmText(song)}</div>
+                    <div
+                      title={`通常ノーツ:${song.n} 皿:${song.s}\nLN:${song.ln} BSS:${song.ls}`}
+                    >
+                      {song.notes}notes
+                    </div>
+                    <div>{getTimeString(song)}</div>
+                  </div>
                   <div>
-                    <div title={`通常ノーツ:${song.n} 皿:${song.s}\nLN:${song.ln} BSS:${song.ls}`}>{song.notes}notes</div>
-                    <div>TOTAL: {Math.floor(song.total)}</div>
                     <div>
-                      判定: {getJudge(song.judge)}({song.judge})
+                      TOTAL: {Math.floor(song.total)}(
+                      {(() => {
+                        const total = Math.floor(song.total);
+                        const calculatedTotal = Math.floor(
+                          calcTotal(song.notes)
+                        );
+                        const diff = total - calculatedTotal;
+                        const sign = diff >= 0 ? "+" : "";
+                        const rate = ((total / calculatedTotal) * 100).toFixed(
+                          2
+                        );
+                        return `${sign + diff}/${rate}%`;
+                      })()}
+                      )
+                    </div>
+                    <div className="info-judge" data-judge={getJudge(song.judge)}>
+                      判定: <span className="info-judge-text">{getJudge(song.judge)}({song.judge})</span>
                     </div>
                   </div>
                   <div>
@@ -77,6 +110,18 @@ export function BMSTable({ table }: Props) {
                     <div>最大: {song.peakdensity}</div>
                     <div>終盤: {song.enddensity}</div>
                   </div>
+                  {(() => {
+                    const features = getFeatures(song);
+                    return features.length ? (
+                      <div className="info-features">
+                        <ul>
+                          {getFeatures(song).map((feature) => (
+                            <li key={feature.id}>{feature.jp}</li>
+                          ))}
+                        </ul>
+                      </div>
+                    ) : null;
+                  })()}
                 </td>
                 <td className="accuracy">
                   <span>{song.accuracy}</span>
